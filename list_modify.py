32381542d9b0530f5e702d1f880579c30f7bc002
agustinportoo <tanitoporto@gmail.com>
2026-05-03 18:00:38 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -12,8 +12,12 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    for indice, valor in enumerate(lst):
+        if valor=="":
+            lst.insert(indice,value)
+            return indice
+    else:
+        return -1
 
 def remove(value, lst):
     """
@@ -26,4 +30,9 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    cant = 0
+    for i in range(len(lst)):
+        if lst[i] == value:
+            lst[i] = ""
+            cant += 1
+    return cant
